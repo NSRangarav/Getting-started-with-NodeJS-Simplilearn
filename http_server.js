41a2b750c0131e1296bb4ea57a408178e9baa17c
Rangaramanujam N S <rangaramanujamsri@gmail.com>
2024-05-12 07:41:14 +0000

    commit

--- http_server.js
+++ http_server.js
@@ -1,5 +1,14 @@
 var http = require('http');
+var express = require('express');
+var app = express();
+
 http.createServer (function(req,res){
     res.write('Hello World! Welcome to my first Nodejs Server!')
     res.end()
+})
+var server = app.listen(8081, function(){
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log("Example App listening at http://%s%s",host,port)
 })
